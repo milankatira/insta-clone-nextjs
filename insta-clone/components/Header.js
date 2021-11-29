7de228efdaa49ff79e82df3f3c6eb14ea43b220d
milankatira <milankatira26@gmail.com>
2021-11-29 09:10:51 +0530

    header-searchbar

---
+++ insta-clone/components/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="flex justify-between bg-white max-w-6xl">
+        {/* left */}
+        <div className="relative hidden lg:inline-grid w-24 h-24">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* middle */}
+        <div>
+          <div></div>
+          <input type="text" placeholder="Search" />
+        </div>
+        {/* right */}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
